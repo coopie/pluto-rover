d831eede09f833c860378f6f14aeec58308db406
Sam Coope <sam.j.coope@gmail.com>
2015-11-04 20:22:34 +0000

    First Test Passes

--- src/rover.js
+++ src/rover.js
@@ -3,15 +3,24 @@ function point(x, y) {
     return {
         x: x,
         y: y
-    }
+    };
 }
 
 function Rover() {
-
+    this.x = 0;
+    this.y = 0;
 }
+
+Rover.prototype.move = function (instructions) {
+    this.y = 1;
+};
+
+Rover.prototype.getPosition = function () {
+    return point(this.x, this.y);
+};
 
 
 module.exports = {
     Rover: Rover,
     point: point
-}
+};
