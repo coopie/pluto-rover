5ec2065187f7c928af8708a68112ea175388f063
Sam Coope <sam.j.coope@gmail.com>
2015-11-04 20:19:56 +0000

    Setup the directory and added first test

---
+++ src/rover.js
@@ -0,0 +1,17 @@
+
+function point(x, y) {
+    return {
+        x: x,
+        y: y
+    }
+}
+
+function Rover() {
+
+}
+
+
+module.exports = {
+    Rover: Rover,
+    point: point
+}
---
+++ test/rover.t.js
@@ -0,0 +1,27 @@
+var assert = require('assert');
+var path = require('path');
+var moduleName = path.basename(__filename).split('.')[0];
+
+var roverModule = require('../src/rover');
+
+var Rover = roverModule.Rover;
+var point = roverModule.point;
+
+var shouldReturn = 'Should return: ';
+
+describe(moduleName, function() {
+    var rover;
+    before(function() {
+        rover = new Rover();
+    })
+
+    describe('move', function() {
+        describe('moves forward one correctly', function() {
+            it(shouldReturn + '{x: 0, y: 1}', function() {
+                rover.move('F');
+                assert.deepEqual(rover.getPosition(), point(0,1));
+            });
+        });
+    });
+
+});
